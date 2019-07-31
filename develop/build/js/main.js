038af5eb8910395b470c715d3f524e5f4fbf6611
user1 <doantam8924@gmail.com>
2019-07-31 17:57:53 +0900

    update

--- develop/build/js/main.js
+++ develop/build/js/main.js
@@ -10,7 +10,16 @@ if(!(k&&k.isSVG||"tween"===l||z.Names.prefixCheck(l)[1]!==!1||z.Normalizations.r
 $(window).on('resize',function(){
   // resize();
 })
-//scroll//
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 1400) {
+        $('#header').addClass('fixed-header');
+    }
+    else {
+        $('#header').removeClass('fixed-header');
+
+    }
+});
+
 $("#js-menuButton").on("click", function() {
  $(this).toggleClass("active");
  $("#js-menu").toggle();
@@ -25,14 +34,12 @@ $("#js-menuButton").on("click", function() {
  return false;
 });
 
-
-
-// Anchor
-$('a[href^="#"]').click(function(){
-  var hasttop = $(this.hash).offset().top;
-  var hasttopNew = hasttop - 104;
-  $('html, body').animate({
-    scrollTop: hasttopNew
-  }, 500);
-  return false;
-});
+// // Anchor
+// $('a[href^="#"]').click(function(){
+//   var hasttop = $(this.hash).offset().top;
+//   var hasttopNew = hasttop - 104;
+//   $('html, body').animate({
+//     scrollTop: hasttopNew
+//   }, 500);
+//   return false;
+// });
